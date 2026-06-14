00524b20bbe4fa45f3da2a9ee654af693119f202
AnandSelvam04 <psananths@gmail.com>
2026-06-14 20:00:45 +0530

    add Add class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("2nd branch");
+﻿using ConsoleApp1;
+using System.ComponentModel;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("2nd branch");
+            Add add = new Add();
+            add.Addition(4, 5);
+        }
+    }
+}
+
+
+
 
